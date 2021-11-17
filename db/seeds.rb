be6d6b63f5f3bea6cf5b7f9672cba29e0fa41c18
Alyssa Coelho <alyssagcoelho@gmail.com>
2021-11-17 15:42:46 -0300

    models ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sushi = Restaurant.create([{ name: "Sushi"}, {address: "7 Boundary St, Japan E2 7JE"}, {category: "japanese"}])
+pizza =  Restaurant.create([{ name: "pizza"}, {address: "8 Boundary St, Italy E2 7JE"}, {category: "italian"}])
+chinese =  Restaurant.create([{ name: "ChinainBox"}, {address: "9 Boundary St, China E2 7JE"}, {category: "Chinese"}])
